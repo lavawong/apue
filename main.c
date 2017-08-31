fa6561e9b514343ecdc023c30bf4c768cfc6bc82
LavaWong <lazy.false@gmail.com>
2017-08-31 13:50:33 +0800

    fix hostname null pointer problem

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 // Created by LavaWong on 6/21/17.
 //
 #include <stdio.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+
 int main(){
-    printf("This is cmake project");
+    printf("This is apue project");
     return 0;
 }
--- sys/hostname.c
+++ sys/hostname.c
@@ -17,31 +17,28 @@
  */
 
 #include "apue.h"
-#include <stdlib.h>
-#include <unistd.h>
-
 #ifndef __USE_POSIX
 #define __USE_POSIX
 #endif
-
 #include <limits.h>
 
-
 int
-main(void)
-{
-    char    *hostname = NULL;
-    long    host_name_max;
+main(void) {
+    char   *host_name = NULL;
+    size_t host_name_max;
 #ifdef _SC_HOST_NAME_MAX
     host_name_max = sysconf(_SC_HOST_NAME_MAX);
 #elif _POSIX_HOST_NAME_MAX
     host_name_max = _POSIX_HOST_NAME_MAX;
 #endif
-    if (host_name_max <=0 )
+    if (host_name_max <= 0)
         host_name_max = 64; /* guess linux 3.2.0 is defined 64*/
-    
-    if (gethostname(hostname, host_name_max) != -1) {
-        printf("hostname: %s\n", hostname);
+    if ((host_name = malloc(host_name_max)) == NULL ) {
+        printf("malloc error!\n");
+    }
+    int code = gethostname(host_name, host_name_max);
+    if (code != -1) {
+        printf("hostname: %s\n", host_name);
     } else {
         printf("Get hostname failure!\n");
     }
