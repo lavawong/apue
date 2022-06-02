5397535553dc3b9eb3ebc74cfef8fa0a6735550f
Lava Wong <lazy.false@gmail.com>
2022-06-02 15:00:59 +0800

    feat: delete makefile

--- ics/access_patterns.c
+++ ics/access_patterns.c
@@ -4,28 +4,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-void init(int src[2048][2048]) {
-    int i, j;
-    for (i = 0; i < 2048; i++) {
-        for (j = 0; j < 2048; j++) {
-            src[i][j] = i + j;
+int** malloc2darr(int m, int n) {
+    int** arr = (int**)malloc(m * sizeof(int*));
+    for (int i = 0; i < m; i++) {
+        *(arr + i) = malloc(n * sizeof(int));
+    }
+    return arr;
+}
+void free2darr(int** arr, int m) {
+    for (int i = 0; i < m; i++) {
+        if (*arr != NULL) {
+            free(*(arr + i));
         }
     }
+    free(arr);
 }
-void copyij(int src[2048][2048], int dst[2048][2048]) {
+
+int** init(int m, int n) {
     int i, j;
-    for (i = 0; i < 2048; i++) {
-        for (j = 0; j < 2048; j++) {
-            dst[i][j] = src[i][j];
+    int** arr = malloc2darr(m, n);
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            *(*(arr + i) + j) = i + j;
+        }
+    }
+    return arr;
+}
+void copyij(int** src, int** dst, int size) {
+    int i, j;
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            *(*(dst + i) + j) = *(*(src + i) + j);
         }
     }
 }
 
-void copyji(int src[2048][2048], int dst[2048][2048]) {
+void copyji(int** src, int** dst, int size) {
     int i, j;
-    for (j = 0; j < 2048; j++) {
-        for (i = 0; i < 2048; i++) {
-            dst[i][j] = src[i][j];
+    for (j = 0; j < size; j++) {
+        for (i = 0; i < size; i++) {
+            *(*(dst + i) + j) = *(*(src + i) + j);
         }
     }
 }
@@ -36,26 +54,37 @@ int main(void) {
 
     // time(&rawtime);
     struct timespec start, end;
-    int i = 2048;
+    int size = 2048;
 
-    int(*src)[i][i] = malloc(sizeof(*src));
-    int(*dst)[i][i] = malloc(sizeof(*dst));
-    init(*src);
+    int** dst = malloc2darr(size, size);
+    // malloc2darr(src, i, i, sizeof(int));
+    // malloc2darr(dst, i, i, sizeof(int));
+    int** src = init(size, size);
     uint64_t delta_us1, delta_us2;
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    copyij(*src, *dst);
+    copyij(src, dst, size);
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     delta_us1 = (end.tv_nsec - start.tv_nsec) * 1e-6 +
                 (end.tv_sec - start.tv_sec) * 1000;
-    printf("copyij took %lu ms\n", delta_us1);
+    printf("copyij took %ld ms\n", delta_us1);
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    copyji(*src, *dst);
+    copyji(src, dst, size);
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     delta_us2 = (end.tv_nsec - start.tv_nsec) * 1e-6 +
                 (end.tv_sec - start.tv_sec) * 1000;
-    printf("copyji took %lu ms\n", delta_us2);
-    free(src);
-    free(dst);
+    printf("copyji took %ld ms\n", delta_us2);
+    // for (int i = 0; i < size; i++) {
+    //     for (int j = 0; j < size; j++) {
+    //         if (j != 0 && j % 8 == 0) {
+    //             printf("\n");
+    //         }
+    //         printf("%10d ", *(*(dst + i) + j));
+    //     }
+    //     printf("\n");
+    // }
+    free2darr(src, size);
+    free2darr(dst, size);
+    return 0;
 }
